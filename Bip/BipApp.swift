62bf13da97b57bf3e227d01d3cfdba08654eab5c
Kamila Hernandez <kaminicoledez@gmail.com>
2026-04-16 09:45:07 -0700

    Initial Commit

---
+++ Bip/BipApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipApp.swift
+//  Bip
+//
+//  Created by HERNANDEZ-CALIX, KAMILA on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct BipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
